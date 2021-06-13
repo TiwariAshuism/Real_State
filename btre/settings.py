4c40da0a3ab3c0fe71d8917acbe71785494066eb
Ashutosh Kumar <aksamtiwari@gmail.com>
2021-06-13 08:01:10 +0530

    final changes

--- btre/settings.py
+++ btre/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ubsq#xai!qa_!dsi=61a&mbqn)lxtl*x48$fi0v-l^k%0z1^bn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
